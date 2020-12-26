b764d9f9dc3e3e81ab0000496f4f480c72156581
Philippe Roemer <philipperoemer@yahoo.com>
2020-12-26 06:13:21 -0500

    digital clock added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Clock from 'react-digital-clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<h1>Tablet Clock - Testing</h1>
+        <h1>Tablet Clock - Testing</h1>
+        <Clock format= {'hh-mm'} />
       </header>
     </div>
   );
