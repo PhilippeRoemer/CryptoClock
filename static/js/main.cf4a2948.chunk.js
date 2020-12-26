df826c2adaf80f2f68b684da4a7a52fe5715b1ef
Philippe Roemer <philipperoemer@yahoo.com>
2020-12-26 06:13:39 -0500

    Updates

--- static/js/main.cf4a2948.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptablet_clock=this.webpackJsonptablet_clock||[]).push([[0],{10:function(e,t,c){},11:function(e,t,c){"use strict";c.r(t);var n=c(0),s=c(1),r=c.n(s),a=c(3),i=c.n(a);c(9),c(10);var l=function(){return Object(n.jsx)("div",{className:"App",children:Object(n.jsx)("header",{className:"App-header",children:Object(n.jsx)("h1",{children:"Tablet Clock - Testing"})})})};i.a.render(Object(n.jsx)(r.a.StrictMode,{children:Object(n.jsx)(l,{})}),document.getElementById("root"))},9:function(e,t,c){}},[[11,1,2]]]);
-//# sourceMappingURL=main.cf4a2948.chunk.js.map
