beba92db27906a7bdda9c1ed273b14cc0727612d
Philippe Roemer <philipperoemer@yahoo.com>
2020-12-27 16:41:02 -0500

    crypto chart updated

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -15,17 +15,17 @@ const Coin = ({
               <td><h3>{name}</h3></td>
               <td><p>${price.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</p></td>
               <td>
-                  {priceChange < 0 ? (
-                      <p className='red'>{priceChange.toFixed(2)}%</p>
-                    ) : (
-                      <p className='green'>{priceChange.toFixed(2)}%</p>
-                    )}
-              </td>
-              <td>
                   {hpriceChange < 0 ? (
                       <p className='red'>{hpriceChange.toFixed(2)}%</p>
                     ) : (
                       <p className='green'>{hpriceChange.toFixed(2)}%</p>
+                    )}
+              </td>
+              <td>
+                  {priceChange < 0 ? (
+                      <p className='red'>{priceChange.toFixed(2)}%</p>
+                    ) : (
+                      <p className='green'>{priceChange.toFixed(2)}%</p>
                     )}
               </td>
               <td>${ath.toLocaleString()}</td>    
