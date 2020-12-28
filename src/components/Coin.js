3c9e3d7508e77834ec0968fde8f9648ada1d4cdf
Philippe Roemer <philipperoemer@yahoo.com>
2020-12-28 13:54:42 -0500

    crypto chart updated

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -15,18 +15,26 @@ const Coin = ({
               <td><h3>{name}</h3></td>
               <td><p>${price.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</p></td>
               <td>
-                  {hpriceChange < 0 ? (
-                      <p className='red'>{hpriceChange.toFixed(2)}%</p>
-                    ) : (
-                      <p className='green'>{hpriceChange.toFixed(2)}%</p>
-                    )}
+                      {hpriceChange < 0 && hpriceChange > -10 ? (
+                            <p className='red'>{hpriceChange.toFixed(2)}%</p> 
+                        ) : hpriceChange < -10  ? (
+                            <p className='redBold'>{hpriceChange.toFixed(2)}%</p>   
+                        ) : hpriceChange < 10 && hpriceChange > 0 ? (
+                            <p className='green'>{hpriceChange.toFixed(2)}%</p>
+                        ) : hpriceChange > 10 (
+                            <p className='greenBold'>{hpriceChange.toFixed(2)}%</p>
+                        )}
               </td>
               <td>
-                  {priceChange < 0 ? (
-                      <p className='red'>{priceChange.toFixed(2)}%</p>
-                    ) : (
-                      <p className='green'>{priceChange.toFixed(2)}%</p>
-                    )}
+                      {priceChange < 0 && priceChange > -10 ? (
+                            <p className='red'>{priceChange.toFixed(2)}%</p> 
+                        ) : priceChange < -10  ? (
+                            <p className='redBold'>{priceChange.toFixed(2)}%</p>   
+                        ) : priceChange < 10 && priceChange > 0 ? (
+                            <p className='green'>{priceChange.toFixed(2)}%</p>
+                        ) : (
+                            <p className='greenBold'>{priceChange.toFixed(2)}%</p>
+                        )}
               </td>
               <td>${ath.toLocaleString()}</td>    
               <td>${marketcap.toLocaleString()}</td>
